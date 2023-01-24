1c72ab784a5af88d11a0d0c4c14a57097c7bf0e0
Joel Jossie <joeljossie@gmail.com>
2023-01-24 09:34:26 -0700

    add some swag

--- app.js
+++ app.js
@@ -5,8 +5,20 @@ let app = express();
 
 // Middlewares / Routes
 app.use(express.json());
+
+let whitelist = ['https://cse341-contacts-frontend.netlify.app'];
+let corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+};
+
+app.use(cors(corsOptions));
 app.use('/', require('./routes'));
-app.use(cors());
 
 
 // Modules
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,9 @@
 const routes = require('express').Router();
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../swagger-output.json');
+
+routes.use('/api-docs', swaggerUi.serve);
+routes.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
 const myController = require('../controllers');
 
